4bb3c0864f0982c67a6441e4b44cb51d0f6b1485
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2025-01-15 15:41:48 -0500

    Adding icons + images of the project

---
+++ src/assets/imageImports.ts
@@ -0,0 +1,47 @@
+import mainScreen1 from "./images/neurosimulation-project/Main_screen1.jpg";
+import mainScreen2 from "./images/neurosimulation-project/Main_screen2.jpg";
+import mainScreen3 from "./images/neurosimulation-project/Main_screen3.jpg";
+import sessionHistory from "./images/neurosimulation-project/Session_history.png";
+import sessionSummary from "./images/neurosimulation-project/Session_summary.jpg";
+import testingMachineLimits from "./images/neurosimulation-project/Testing_machine_limits.jpg";
+import uploadingData from "./images/neurosimulation-project/Uploading_data.jpg";
+import systemDesign from "./images/neurosimulation-project/System_design.jpg";
+import portfolioPage from "./images/portfolio-site/portfolio-page.jpg";
+import thumbnailProject1 from "./images/neurosimulation-project/Thumbnail-project1.jpg";
+import systemOperation from "./images/neurosimulation-project/System_operation.jpg";
+import thumbnailProject3 from "./images/database-comparator-project/throughput_run_comparison.png"
+import thumbnailProject4 from "./images/machine-learning-project/machine-learning.jpg"
+
+const images = {
+    projects: {
+        neurosimulationProject: {
+            mainScreen1,
+            mainScreen2,
+            mainScreen3,
+            sessionHistory,
+            sessionSummary,
+            testingMachineLimits,
+            uploadingData,
+            systemDesign,
+            thumbnailProject1,
+            systemOperation,
+            thumbnailProject3,
+            thumbnailProject4
+        },
+        project1: {
+            thumbnail: thumbnailProject1,
+        },
+        project2: {
+            thumbnail: portfolioPage,
+        },
+        project3: {
+            thumbnail: thumbnailProject3,
+        },
+        project4: {
+            thumbnail: thumbnailProject4,
+        },
+    },
+};
+
+export default images;
+
---
+++ src/utils/icons.ts
@@ -0,0 +1,70 @@
+// Importing icons from React Icons
+import {
+    FaPython,
+    FaJava,
+    FaReact,
+    FaAngular,
+    FaDocker,
+    FaAws,
+    FaGit,
+    FaMap,
+    FaTerminal,
+    FaChartBar
+} from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import {
+    SiPostgresql,
+    SiMongodb,
+    SiTensorflow,
+    SiScikitlearn,
+    SiSpringboot,
+    SiTypescript,
+    SiFastapi,
+    SiKotlin,
+    SiRedis, SiPandas
+} from "react-icons/si";
+import { FaHome, FaTasks, FaTools, FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsFiletypeSql } from "react-icons/bs";
+
+
+export const Icons = {
+    // Programming language
+    Java: FaJava,
+    Python: FaPython,
+    TypeScript: SiTypescript,
+    JavaScript: DiJavascript1,
+    SQL: BsFiletypeSql,
+
+    // Web and IA
+    React: FaReact,
+    Angular: FaAngular,
+    SpringBoot: SiSpringboot,
+    TensorFlow: SiTensorflow,
+    ScikitLearn: SiScikitlearn,
+    FastAPI: SiFastapi,
+    Kotlin: SiKotlin,
+    Matplotlib: FaChartBar,
+    Pandas: SiPandas,
+    SkLearn: SiScikitlearn,
+
+
+    // Databases
+    PostgreSQL: SiPostgresql,
+    MongoDB: SiMongodb,
+    Redis:  SiRedis,
+
+    // Tools and technologies
+    Docker: FaDocker,
+    AWS: FaAws,
+    Git: FaGit,
+    Bash: FaTerminal,
+
+    // Navbar icons
+    Home: FaHome,
+    Tasks: FaTasks,
+    Tools: FaTools,
+    Envelope: FaEnvelope,
+    LinkedIn: FaLinkedin,
+    GitHub: FaGithub,
+    Map: FaMap,
+};
